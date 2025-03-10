e089f27b048ccd3811e074489836acdf7fefc31a
Diegom-95 <diegomaggioni95@gmail.com>
2025-03-10 19:14:15 +0100

    added js project in portfolio section

---
+++ README.md
@@ -0,0 +1,5 @@
+Diego Maggioni's Personal Portfolio Website
+
+[Website](https://diegom-95.github.io/Portfolio/)
+
+[Presentation](https://drive.google.com/file/d/1pr4pSg9BVvuz5lDFxDmRyw06Df_D8xVf/view?usp=drive_link) 
---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+function sendMail(){
+  let parms = {
+      name : document.getElementById("name").value,
+      email : document.getElementById("email").value,
+      subject : document.getElementById("subject").value,
+      message : document.getElementById("message").value,
+  }
+
+
+  emailjs.send("service_5nykcva","template_raj2ueb",parms).then(alert("Email Sent!"))
+}
